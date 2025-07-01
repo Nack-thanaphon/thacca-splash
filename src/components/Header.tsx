7ee0371e22e8fd5cf65402f76587c556f25aa66f
thanaphon <e21bvz@gmail.com>
2025-07-01 10:13:02 +0700

    Enhance CSS transitions and update Header component padding for improved layout
    - Added vendor prefixes for transform property in index.css to ensure cross-browser compatibility.
    - Increased padding in Header component for better spacing and layout consistency.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,7 +5,7 @@ import { HeroSection } from './section'
 
 export const Header = ({ params }: { params: { lang: string } }) => {
     return (
-        <div className=' h-full bg-red-500 pt-2 w-full relative'>
+        <div className=' h-full bg-red-500 pt-2 w-full relative p-4'>
             <div className='lg:absolute top-0 right-0 flex justify-center items-center  p-4'>
                 <LanguageSwitcher currentLocale={params.lang} />
             </div>
